32a41cf5019ad0168a1eb208d956bbacb9e528e3
Youssef Al Mostafa <youssefalmostafa2@gmail.com>
2025-05-10 13:47:35 +0300

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Banner from './components/Banner.vue'
 import WeatherPage from './components/WeatherPage.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Banner />
   <WeatherPage />
+  <Footer />
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,7 +14,6 @@ const updateDateTime = () => {
       day: 'numeric',
     })
 }
-
 onMounted(() => {
   updateDateTime();
 })
@@ -29,13 +28,6 @@ onMounted(() => {
         <p class="menu-a text-[#333333] font-bold text-[24px] font-sans">{{ currentDate }}</p>
       </div>
       <div class="nav-accounts-btn text-lg text-black flex gap-5">
-        <a href="mailto:youssefalmostafa2@gmail.com">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" class="bi bi-envelope"
-            viewBox="0 0 16 16">
-            <path
-              d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
-          </svg>
-        </a>
         <a href="https://linkedin.com/in/youssef-al-mostafa" target="_blank">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" class="bi bi-linkedin"
             viewBox="0 0 16 16">
